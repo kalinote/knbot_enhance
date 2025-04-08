8f955575fa02ef0ba981d8efdfd324a44f94176e
kalinote <kalinote@163.com>
2025-04-08 18:02:57 +0800

    优化 Markdown 图片生成逻辑，增加生成回复总结的功能，并调整相关配置项和日志记录信息。

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from astrbot.api.message_components import ComponentType
 from playwright.async_api import async_playwright
 from jinja2 import Template
 
-@register("knbot_enhance", "Kalinote", "[自用]KNBot 功能增强插件", "0.0.4", "https://github.com/kalinote/knbot_enhance")
+@register("knbot_enhance", "Kalinote", "[自用]KNBot 功能增强插件", "0.0.5", "https://github.com/kalinote/knbot_enhance")
 class KNBotEnhance(Star):
     """[自用]KNBot 功能增强插件
     """
@@ -27,16 +27,18 @@ class KNBotEnhance(Star):
         if self.config.get("markdown_image_generate").get("enable"):
             html_path = os.path.join(os.getcwd(), os.path.join("data", "plugins", "knbot_enhance", "resource"), "markdown-template.html")
             if not os.path.exists(html_path):
-                logger.error(f"样式文件不存在: {html_path}")
+                logger.error(f"Markdown渲染模板文件不存在: {html_path}")
             else:
                 try:
                     with open(html_path, 'r', encoding='utf-8') as f:
                         self.markdown_html_template = Template(f.read())
                     logger.debug(f"载入模板文件: {html_path}")
                 except Exception as e:
-                    logger.warning(f"读取样式文件失败: {e}; 相关功能已禁用")
+                    logger.warning(f"读取Markdown渲染文件失败: {e}; 相关功能已禁用")
                     self.config["markdown_image_generate"]["enable"] = False
                     self.config.save()
+                    
+        self.topic_summary_prompt = "你是一名擅长内容总结的助理，你需要将用户的内容总结为 10 个字以内的标题，标题语言与用户的首要语言一致，不要使用标点符号和其他特殊符号。直接返回总结内容，不要有其他内容。"
     
     @filter.on_decorating_result(desc="将过长的文本内容转换为Markdown图片")
     async def markdown_image_generate(self, event: AstrMessageEvent):
@@ -47,14 +49,24 @@ class KNBotEnhance(Star):
                 # TODO 这里可以进一步优化，而不只是简单通过字数来判断
                 if item.type == ComponentType.Plain.value and len(item.text) > self.config.get("markdown_image_generate").get("trigger_count"):
                     logger.info(f"将文本内容转换为Markdown图片: {item.text[:10]}...")
-                    image_path = await self.text_to_markdown_image(item.text)
+                    image_path = await self.text_to_markdown_image(item.text, self.config.get("markdown_image_generate").get("generate_topic_summary"))
                     if not image_path:
                         continue
                     
                     logger.info(f"生成Markdown图片: {image_path}")
                     chain[index] = Comp.Image.fromFileSystem(image_path)
 
-    async def text_to_markdown_image(self, text: str) -> str:
+    async def generate_topic_summary(self, text: str) -> str:
+        """
+        生成会话总结
+        """
+        response = await self.context.get_using_provider().text_chat(
+            prompt=text,
+            system_prompt=self.topic_summary_prompt,
+        )
+        return response.completion_text
+
+    async def text_to_markdown_image(self, text: str, generate_topic_summary: bool = False) -> str:
         """
         将 Markdown 文本转换为带有样式的图片（使用模板文件）
         """
@@ -67,7 +79,8 @@ class KNBotEnhance(Star):
             # 生成完整 HTML
             json_encoded_text = json.dumps(text)
             full_html = self.markdown_html_template.render(
-                json_text="const markdownInput = " + json_encoded_text + ";",       # 传递markdown
+                json_text="const markdownInput = " + json_encoded_text + ";",
+                topic_summary= await self.generate_topic_summary(text) if generate_topic_summary else "KNBot Enhance",
             )
 
             # 文件保存路径
@@ -76,6 +89,8 @@ class KNBotEnhance(Star):
             file_name = f"{hashlib.md5(full_html.encode('utf-8')).hexdigest()}.png"
             output_path = os.path.join(output_dir, file_name)
             
+            with open(f"{output_path}.txt", "w", encoding="utf-8") as f:
+                f.write(text)
             with open(f"{output_path}.html", "w", encoding="utf-8") as f:
                 f.write(full_html)
 
